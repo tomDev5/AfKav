6aeb58fc03dcbe40f968474273e8e8697c73d611
Tom Lubin <tom@lubin.ws>
2022-08-20 00:35:35 +0300

    works

--- app/src/main/java/com/randomhq/afkav/MainActivity.kt
+++ app/src/main/java/com/randomhq/afkav/MainActivity.kt
@@ -1,32 +1,212 @@
 package com.randomhq.afkav
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.MifareClassic
+import android.nfc.tech.MifareUltralight
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import android.os.Parcelable
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.randomhq.afkav.databinding.ActivityMainBinding
+import kotlin.experimental.and
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    private var isScanning = false  // True = Scanning, False = Transmitting.
+
+    private lateinit var nfcAdapter: NfcAdapter  // The NFC Adapter the phone has.
+    private lateinit var pendingIntent: PendingIntent  // Pending Intent to receive an NFC card.
+
+    private lateinit var addCardButton: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Transmit Activity
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        // binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
 
-        setSupportActionBar(binding.toolbar)
+        // setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        // val navController = findNavController(R.id.nav_host_fragment_content_main)
+        // appBarConfiguration = AppBarConfiguration(navController.graph)
+        // setupActionBarWithNavController(navController, appBarConfiguration)
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this, this.javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+            PendingIntent.FLAG_MUTABLE
+        )
+
+        addCardButton = findViewById(R.id.addCardButton)
+
+        addCardButton.setOnClickListener {
+            isScanning = !isScanning
+            val isEnabledStr = if (isScanning) "enabled" else "disabled"
+            val message = "Scanning now $isEnabledStr"
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null)
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter.disableForegroundDispatch(this)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent != null) {
+            Toast.makeText(this, intent.getAction().toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        setIntent(intent)
+        if (intent != null) {
+            resolveIntent(intent)
+        }
+    }
+
+    private fun resolveIntent(intent: Intent) {
+        val action = intent.action
+        if (NfcAdapter.ACTION_TAG_DISCOVERED == action ||
+            NfcAdapter.ACTION_TECH_DISCOVERED == action ||
+            NfcAdapter.ACTION_NDEF_DISCOVERED == action
+        ) {
+            val tag: Tag = (intent.getParcelableExtra<Parcelable>(NfcAdapter.EXTRA_TAG) as Tag?)!!
+            val payload: String = detectTagData(tag)
+
+            // Save the NFC card to Shared Prefs
+            saveNfcCard(payload)
+        }
+    }
+
+    private fun saveNfcCard(cardData: String) {
+        Toast.makeText(this, "Saving...", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun detectTagData(tag: Tag): String {
+        val sb = StringBuilder()
+        val id = tag.id
+        sb.append("ID (hex): ").append(toHex(id)).append('\n')
+        sb.append("ID (reversed hex): ").append(toReversedHex(id)).append('\n')
+        sb.append("ID (dec): ").append(toDec(id)).append('\n')
+        sb.append("ID (reversed dec): ").append(toReversedDec(id)).append('\n')
+        val prefix = "android.nfc.tech."
+        sb.append("Technologies: ")
+        for (tech in tag.techList) {
+            sb.append(tech.substring(prefix.length))
+            sb.append(", ")
+        }
+        sb.delete(sb.length - 2, sb.length)
+        for (tech in tag.techList) {
+            if (tech == MifareClassic::class.java.name) {
+                sb.append('\n')
+                var type = "Unknown"
+                try {
+                    val mifareTag = MifareClassic.get(tag)
+                    when (mifareTag.type) {
+                        MifareClassic.TYPE_CLASSIC -> type = "Classic"
+                        MifareClassic.TYPE_PLUS -> type = "Plus"
+                        MifareClassic.TYPE_PRO -> type = "Pro"
+                    }
+                    sb.append("Mifare Classic type: ")
+                    sb.append(type)
+                    sb.append('\n')
+                    sb.append("Mifare size: ")
+                    sb.append(mifareTag.size.toString() + " bytes")
+                    sb.append('\n')
+                    sb.append("Mifare sectors: ")
+                    sb.append(mifareTag.sectorCount)
+                    sb.append('\n')
+                    sb.append("Mifare blocks: ")
+                    sb.append(mifareTag.blockCount)
+                } catch (e: Exception) {
+                    sb.append("Mifare classic error: " + e.message)
+                }
+            }
+            if (tech == MifareUltralight::class.java.name) {
+                sb.append('\n')
+                val mifareUlTag = MifareUltralight.get(tag)
+                var type = "Unknown"
+                when (mifareUlTag.type) {
+                    MifareUltralight.TYPE_ULTRALIGHT -> type = "Ultralight"
+                    MifareUltralight.TYPE_ULTRALIGHT_C -> type = "Ultralight C"
+                }
+                sb.append("Mifare Ultralight type: ")
+                sb.append(type)
+            }
+        }
+        Log.v("test", sb.toString())
+        return sb.toString()
+    }
+
+    private fun toHex(bytes: ByteArray): String {
+        val sb = StringBuilder()
+        for (i in bytes.indices.reversed()) {
+            val b: Byte = bytes[i] and 0xff.toByte()
+            if (b < 0x10) sb.append('0')
+            sb.append(Integer.toHexString(b.toInt()))
+            if (i > 0) {
+                sb.append(" ")
+            }
+        }
+        return sb.toString()
+    }
+
+    private fun toReversedHex(bytes: ByteArray): String {
+        val sb = StringBuilder()
+        for (i in bytes.indices) {
+            if (i > 0) {
+                sb.append(" ")
+            }
+            val b: Byte = bytes[i] and 0xff.toByte()
+            if (b < 0x10) sb.append('0')
+            sb.append(Integer.toHexString(b.toInt()))
+        }
+        return sb.toString()
+    }
+
+    private fun toDec(bytes: ByteArray): Long {
+        var result: Long = 0
+        var factor: Long = 1
+        for (i in bytes.indices) {
+            val value: Byte = bytes[i] and 0xffL.toByte()
+            result += value * factor
+            factor *= 256L
+        }
+        return result
+    }
+
+    private fun toReversedDec(bytes: ByteArray): Long {
+        var result: Long = 0
+        var factor: Long = 1
+        for (i in bytes.indices.reversed()) {
+            val value: Byte = bytes[i] and 0xffL.toByte()
+            result += value * factor
+            factor *= 256L
+        }
+        return result
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -45,9 +225,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
