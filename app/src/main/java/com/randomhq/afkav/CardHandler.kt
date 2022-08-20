c5e2d0df694a338f72c408d64ffe591b03948ad8
tomDev5 <tom@lubin.ws>
2022-08-20 12:25:51 +0300

    added UI

--- app/src/main/java/com/randomhq/afkav/CardHandler.kt
+++ app/src/main/java/com/randomhq/afkav/CardHandler.kt
@@ -1,10 +1,83 @@
 package com.randomhq.afkav
 
-fun saveCard(cardData: String) {
-    // Toast.makeText(this, "Saving...", Toast.LENGTH_SHORT).show()
+import android.content.Context
+import android.nfc.Tag
+import android.util.Log
+import com.google.gson.Gson
+
+private var TAG_ID_PREFERENCES_KEY: String = "TAG_ID"
+private val gson = Gson()
+
+fun saveCard(context: Context, tag: Tag) = context.getSharedPreferences(
+    context.packageName, Context.MODE_PRIVATE
+).edit().putString(TAG_ID_PREFERENCES_KEY, gson.toJson(tag)).apply()
+
+fun loadCard(context: Context): Tag? {
+    val json: String? = context.getSharedPreferences(
+        context.packageName, Context.MODE_PRIVATE
+    ).getString(
+        TAG_ID_PREFERENCES_KEY,
+        ""
+    )
+    return gson.fromJson(json, Tag::class.java) ?: return null
 }
 
-private fun loadCard(): String {
-    // Toast.makeText(, "Loading...", Toast.LENGTH_SHORT).show()
-    return ""
-}
+fun removeCard(context: Context) = context.getSharedPreferences(
+    context.packageName, Context.MODE_PRIVATE
+).edit().clear().apply()
+
+//private fun debug_detectTagData(tag: Tag): String {  // TODO: Move to other class
+//    val sb = StringBuilder()
+//    val id = tag.id
+//    sb.append("ID (hex): ").append(toHex(id)).append('\n')
+//    sb.append("ID (reversed hex): ").append(toReversedHex(id)).append('\n')
+//    sb.append("ID (dec): ").append(toDec(id)).append('\n')
+//    sb.append("ID (reversed dec): ").append(toReversedDec(id)).append('\n')
+//    val prefix = "android.nfc.tech."
+//    sb.append("Technologies: ")
+//    for (tech in tag.techList) {
+//        sb.append(tech.substring(prefix.length))
+//        sb.append(", ")
+//    }
+//    sb.delete(sb.length - 2, sb.length)
+//    for (tech in tag.techList) {
+//        if (tech == MifareClassic::class.java.name) {
+//            sb.append('\n')
+//            var type = "Unknown"
+//            try {
+//                val mifareTag = MifareClassic.get(tag)
+//                when (mifareTag.type) {
+//                    MifareClassic.TYPE_CLASSIC -> type = "Classic"
+//                    MifareClassic.TYPE_PLUS -> type = "Plus"
+//                    MifareClassic.TYPE_PRO -> type = "Pro"
+//                }
+//                sb.append("Mifare Classic type: ")
+//                sb.append(type)
+//                sb.append('\n')
+//                sb.append("Mifare size: ")
+//                sb.append(mifareTag.size.toString() + " bytes")
+//                sb.append('\n')
+//                sb.append("Mifare sectors: ")
+//                sb.append(mifareTag.sectorCount)
+//                sb.append('\n')
+//                sb.append("Mifare blocks: ")
+//                sb.append(mifareTag.blockCount)
+//            } catch (e: Exception) {
+//                sb.append("Mifare classic error: " + e.message)
+//            }
+//        }
+//        if (tech == MifareUltralight::class.java.name) {
+//            sb.append('\n')
+//            val mifareUlTag = MifareUltralight.get(tag)
+//            var type = "Unknown"
+//            when (mifareUlTag.type) {
+//                MifareUltralight.TYPE_ULTRALIGHT -> type = "Ultralight"
+//                MifareUltralight.TYPE_ULTRALIGHT_C -> type = "Ultralight C"
+//            }
+//            sb.append("Mifare Ultralight type: ")
+//            sb.append(type)
+//        }
+//    }
+//    Log.v("test", sb.toString())
+//    return sb.toString()
+//}
--- app/src/main/java/com/randomhq/afkav/MainActivity.kt
+++ app/src/main/java/com/randomhq/afkav/MainActivity.kt
@@ -4,27 +4,30 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.nfc.NfcAdapter
 import android.nfc.Tag
-import android.nfc.tech.MifareClassic
-import android.nfc.tech.MifareUltralight
 import android.os.Bundle
 import android.os.Parcelable
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import org.w3c.dom.Text
+
+
+
 
 
 class MainActivity : AppCompatActivity() {
+    private var savedTag: Tag? = null
+
     private lateinit var nfcAdapter: NfcAdapter  // The NFC Adapter the phone has.
     private lateinit var pendingIntent: PendingIntent  // Pending Intent to receive an NFC card.
 
     private var isScanning = false  // True = Scanning, False = Transmitting.
     private lateinit var addCardButton: FloatingActionButton
+    private lateinit var removeCardButton: FloatingActionButton
+    private lateinit var transmitButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,10 +46,38 @@ class MainActivity : AppCompatActivity() {
 
         addCardButton.setOnClickListener {
             isScanning = !isScanning
-            val stateTextView = findViewById<TextView>(R.id.state_text_view)
-            stateTextView.text =
-                if (isScanning) getString(R.string.scanning_for_card) else getString(R.string.ready_to_transmit)
+            updateStateTextView()
         }
+
+        removeCardButton = findViewById(R.id.removeCardButton)
+
+        removeCardButton.setOnClickListener {
+            savedTag = null
+            removeCard(applicationContext)
+            updateStateTextView()
+        }
+
+        transmitButton = findViewById(R.id.transmit_button)
+
+        transmitButton.setOnClickListener {
+            Snackbar.make(findViewById(R.id.constraints), "Transmitting", Snackbar.LENGTH_SHORT).show()
+        }
+
+        savedTag = loadCard(applicationContext)
+        updateStateTextView()
+    }
+
+    private fun updateStateTextView() {
+        val stateTextView = findViewById<TextView>(R.id.state_text_view)
+        stateTextView.text = if (isScanning) {
+            getString(R.string.scanning_for_card)
+        } else if (savedTag == null) {
+            getString(R.string.no_card_saved)
+        } else {
+            getString(R.string.ready_to_transmit)
+        }
+
+        transmitButton.isEnabled = !(isScanning || savedTag == null)
     }
 
     override fun onResume() {
@@ -76,67 +107,13 @@ class MainActivity : AppCompatActivity() {
             NfcAdapter.ACTION_NDEF_DISCOVERED == action
         ) {
             val tag: Tag = (intent.getParcelableExtra<Parcelable>(NfcAdapter.EXTRA_TAG) as Tag?)!!
-            val payload: String = detectTagData(tag)
 
             // Save the NFC card to Shared Prefs
-            saveCard(payload)
+            saveCard(applicationContext, tag)
+            isScanning = false
+            savedTag = tag
+            updateStateTextView()
         }
-    }
-
-    private fun detectTagData(tag: Tag): String {  // TODO: Move to other class
-        val sb = StringBuilder()
-        val id = tag.id
-        sb.append("ID (hex): ").append(toHex(id)).append('\n')
-        sb.append("ID (reversed hex): ").append(toReversedHex(id)).append('\n')
-        sb.append("ID (dec): ").append(toDec(id)).append('\n')
-        sb.append("ID (reversed dec): ").append(toReversedDec(id)).append('\n')
-        val prefix = "android.nfc.tech."
-        sb.append("Technologies: ")
-        for (tech in tag.techList) {
-            sb.append(tech.substring(prefix.length))
-            sb.append(", ")
-        }
-        sb.delete(sb.length - 2, sb.length)
-        for (tech in tag.techList) {
-            if (tech == MifareClassic::class.java.name) {
-                sb.append('\n')
-                var type = "Unknown"
-                try {
-                    val mifareTag = MifareClassic.get(tag)
-                    when (mifareTag.type) {
-                        MifareClassic.TYPE_CLASSIC -> type = "Classic"
-                        MifareClassic.TYPE_PLUS -> type = "Plus"
-                        MifareClassic.TYPE_PRO -> type = "Pro"
-                    }
-                    sb.append("Mifare Classic type: ")
-                    sb.append(type)
-                    sb.append('\n')
-                    sb.append("Mifare size: ")
-                    sb.append(mifareTag.size.toString() + " bytes")
-                    sb.append('\n')
-                    sb.append("Mifare sectors: ")
-                    sb.append(mifareTag.sectorCount)
-                    sb.append('\n')
-                    sb.append("Mifare blocks: ")
-                    sb.append(mifareTag.blockCount)
-                } catch (e: Exception) {
-                    sb.append("Mifare classic error: " + e.message)
-                }
-            }
-            if (tech == MifareUltralight::class.java.name) {
-                sb.append('\n')
-                val mifareUlTag = MifareUltralight.get(tag)
-                var type = "Unknown"
-                when (mifareUlTag.type) {
-                    MifareUltralight.TYPE_ULTRALIGHT -> type = "Ultralight"
-                    MifareUltralight.TYPE_ULTRALIGHT_C -> type = "Ultralight C"
-                }
-                sb.append("Mifare Ultralight type: ")
-                sb.append(type)
-            }
-        }
-        Log.v("test", sb.toString())
-        return sb.toString()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
